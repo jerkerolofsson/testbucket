dcdfd81f792eb7c1f3020843b6fdfc8f31076521
Jerker <jerker@ecredence.com>
2025-05-15 16:45:42 +0800

    work

---
+++ src/TestBucket.Domain/Testing/Specifications/TestCaseRuns/FilterTestCaseRunsByBooleanField.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+
+using TestBucket.Domain.Shared.Specifications;
+using TestBucket.Domain.Testing.Models;
+
+namespace TestBucket.Domain.Testing.Specifications.TestCases;
+
+public class FilterTestCaseRunsByBooleanField : FilterSpecification<TestCaseRun>
+{
+    private readonly long _fieldDefinitionId;
+    private readonly bool _value;
+
+    public FilterTestCaseRunsByBooleanField(long fieldDefinitionId, bool value)
+    {
+        _fieldDefinitionId = fieldDefinitionId;
+        _value = value;
+    }
+
+    protected override Expression<Func<TestCaseRun, bool>> GetExpression()
+    {
+        return x => x.TestCaseRunFields!.Where(f => f.FieldDefinitionId == _fieldDefinitionId && f.BooleanValue == _value).Any();
+    }
+}

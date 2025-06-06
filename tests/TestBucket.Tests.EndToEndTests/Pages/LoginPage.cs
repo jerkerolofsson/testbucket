650491295e6f71b56d71d9de90ad5358cd48b1f7
Jerker <jerker@ecredence.com>
2025-06-06 18:29:16 +0800

    test:login

--- tests/TestBucket.Tests.EndToEndTests/Pages/LoginPage.cs
+++ tests/TestBucket.Tests.EndToEndTests/Pages/LoginPage.cs
@@ -9,10 +9,15 @@ namespace TestBucket.Tests.EndToEndTests.Pages
 {
     internal class LoginPage(PlaywrightFixture Fixture)
     {
-        internal async Task LoginAsync(string? email, string? password)
+        internal async Task LoginAsync(string email, string password)
         {
             var page = await Fixture.Browser.NewPageAsync();
-            await page.GotoAsync(Fixture.HttpsBaseUrl);
+            await page.GotoAsync(Fixture.HttpsBaseUrl + "/Login");
+
+            await page.FillAsync("#Input.Email", email);
+            await page.FillAsync("#Input.Password", password);
+
+            await page.ClickAsync("#Input.Login");
 
             await Task.Delay(30000);
             // #Input.Email

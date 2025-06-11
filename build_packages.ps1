d03ea95e3ec29164d2876517d71c5f6fa2b80a02
Jerker <jerker@ecredence.com>
2025-06-11 10:20:24 +0800

    Add readme

--- build_packages.ps1
+++ build_packages.ps1
@@ -1,21 +1,24 @@
-$version="1.0.1"
+$version="1.0.2"
 $package="TestBucket.Traits.Core"
-
 cd src/Package/Traits/${package}
 dotnet pack -p:PackageVersion=$version
-nuget push bin/Release/${package}.${version}.nupkg  			-Source https://api.nuget.org/v3/index.json
+nuget push bin/Release/${package}.${version}.nupkg -Source https://api.nuget.org/v3/index.json
 cd ../../../..
 
 $package="TestBucket.Traits.Xunit"
-
 cd src/Package/Traits/${package}
 dotnet pack -p:PackageVersion=$version
-nuget push bin/Release/${package}.${version}.nupkg  			-Source https://api.nuget.org/v3/index.json
+nuget push bin/Release/${package}.${version}.nupkg -Source https://api.nuget.org/v3/index.json
 cd ../../../..
 
 $package="TestBucket.Traits.TUnit"
-
 cd src/Package/Traits/${package}
 dotnet pack -p:PackageVersion=$version
-nuget push ./bin/Release/${package}.${version}.nupkg  			-Source https://api.nuget.org/v3/index.json
+nuget push ./bin/Release/${package}.${version}.nupkg -Source https://api.nuget.org/v3/index.json
+cd ../../../..
+
+$package="TestBucket.McpTest.Xunit"
+cd src/Package/McpTest/${package}
+dotnet pack -p:PackageVersion=$version
+nuget push ./bin/Release/${package}.${version}.nupkg -Source https://api.nuget.org/v3/index.json
 cd ../../../..

a568d3b319d858e7551af9fb2e8891be8eb42e89
Jerker <jerker@ecredence.com>
2025-07-16 18:18:51 +0800

    Translations

---
+++ src/TestBucket.Domain/AI/Runner/AiRunner.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace TestBucket.Domain.AI.Runner;
+internal class AiRunner : BackgroundService
+{
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        return Task.CompletedTask;
+    }
+}
